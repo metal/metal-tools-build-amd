3f1966a780724829575d89929dac36e6b40299c0
Maira Bello <maira.araujo@liferay.com>
2015-12-09 15:23:31 -0300

    Moves default options to separate file

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 var buildAmd = require('./lib/pipelines/buildAmd');
 var consume = require('stream-consume');
+var defaultOptions = require('./lib/options');
+var merge = require('merge');
 var vfs = require('vinyl-fs');
 
 module.exports = function (options) {
-	options  = options || {};
-	options.base = options.base || process.cwd();
-
+	options = merge({}, defaultOptions, options);
 	var stream = vfs.src(options.src || 'src/**/*.js', {base: options.base})
 		.pipe(buildAmd(options))
 		.pipe(vfs.dest(options.dest || 'build/amd'));
